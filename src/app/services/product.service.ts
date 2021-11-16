3f938b883ac49bd3eb9079da9db984d418196dd5
san k <sankdev0@gmail.com>
2021-11-16 23:15:56 +0500

    Update ProductService to map the REST JSON data
    Now ProductService can map the JSON data from Spring Data REST to Product
    array in method getProductList().
    The baseUrl is hardcoded for now.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  /**
+   * Returns an observable. The method maps the JSON data from Spring Data REST to Product array.
+   */
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    )
+  }
+
+}
+
+/**
+ * Supporting interface. Unwraps the JSON from Spring Data REST _embedded entry.
+ */
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
