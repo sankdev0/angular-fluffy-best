7a4c10560a3953bea224b3036fa352f5a19a1420
san k <sankdev0@gmail.com>
2021-11-16 23:33:59 +0500

    Fix TS error in Product class
    Fir TS Error Property has no initializer and is not definitely assigned:
    make all properties optional

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,14 +1,12 @@
 export class Product {
 
-    constructor(
-        sku: string,
-        name: string,
-        description: string,
-        unitPrice: number,
-        imageUrl: string,
-        active: boolean,
-        unitsInStock: number,
-        dateCreated: Date,
-        lastUpdated: Date
-    ) { }
+        sku?: string;
+        name?: string;
+        description?: string;
+        unitPrice?: number;
+        imageUrl?: string;
+        active?: boolean;
+        unitsInStock?: number;
+        dateCreated?: Date;
+        lastUpdated?: Date;
 }
