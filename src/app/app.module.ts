f5b05c2b2ced96ce5c4594010ddd94907ee7b521
san k <sankdev0@gmail.com>
2021-11-30 09:47:26 +0500

    Add search by category - static

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,17 @@ import { ProductService } from './services/product.service';
 import { LanguageListComponent } from './components/language-list/language-list.component';
 import { LanguageService } from './services/language.service';
 
+import { Routes, RouterModule} from '@angular/router';
+
+// The Order of routes is important
+const routes: Routes = [
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +26,8 @@ import { LanguageService } from './services/language.service';
     LanguageListComponent
   ],
   imports: [
+    // Configure the router in the application module
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,28 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products?: Product[];
+  currentCategoryId?: number;
 
-  // Inject ProductService into ProductListComponent
-  constructor(private productService: ProductService) { }
+  // Inject ProductService and ActivatedRoute into ProductListComponent
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
+    
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. Convert string to a number.
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // not category id is available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
     // Method is invoked once you "subscribe".
-    this.productService.getProductList().subscribe(
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       // Assign results to the Product array.
       data => {
         this.products = data;
