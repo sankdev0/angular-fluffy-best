cb8ccd3e86ba7fe2463e1ede9a14a3e4d76bdcb8
san k <sankdev0@gmail.com>
2022-02-03 15:46:43 +0500

    Add pagination support for search results

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -18,8 +18,10 @@ export class ProductListComponent implements OnInit {
 
   // Props for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 2;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
+
+  previousKeyword: string | null = null;
 
   // Inject ProductService and ActivatedRoute into ProductListComponent
   constructor(private productService: ProductService,
@@ -44,12 +46,21 @@ export class ProductListComponent implements OnInit {
   handleSearchProducts() {
     const theKeyword = this.route.snapshot.paramMap.get('keyword');
 
+    // if we have a different keyword than previous
+    // then set thePageNumber to 1
+
+    if (this.previousKeyword !== theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
     // now search for the products using keyword
-    this.productService.searchProducts(theKeyword!).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+      this.thePageSize,
+      theKeyword!).subscribe(this.processResult());
   }
 
   handleListProducts() {
